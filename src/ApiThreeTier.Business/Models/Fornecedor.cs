b022b2d6d3aad2d038ed2e610a9d52de84de6236
Wesley Alves <wesley.info07@gmail.com>
2024-05-06 17:29:22 -0300

    criando minhas models e validando entrada de dados

---
+++ src/ApiThreeTier.Business/Models/Fornecedor.cs
@@ -0,0 +1,11 @@
+﻿namespace ApiThreeTier.Business.Models
+{
+    public class Fornecedor : Entity
+    {
+        public string? Nome { get; set; }
+        public string? Documento { get; set; }
+        public TipoFornecedor TipoFornecedor { get; set; }
+        public bool Ativo { get; set; }
+        public Endereco? Endereco { get; set; }
+    }
+}
---
+++ src/ApiThreeTier.Business/Models/Produto.cs
@@ -0,0 +1,12 @@
+﻿
+namespace ApiThreeTier.Business.Models
+{
+    public class Produto : Entity
+    {
+        public string? Nome { get; set; }
+        public string? Descricao { get; set; }
+        public decimal? Valor { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public bool Ativo { get; set; }
+    }
+}
