9b38d61beb4b2bb96cc1ee0a7078dd2d0f9f328c
Wesley Alves <wesley.info07@gmail.com>
2024-05-08 22:35:04 -0300

    Definindo contexto do entityFramework

---
+++ src/ApiThreeTier.Data/Context/MeuDbContext.cs
@@ -0,0 +1,17 @@
+﻿using ApiThreeTier.Business.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiThreeTier.Data.Context
+{
+    public class MeuDbContext : DbContext
+    {
+        public MeuDbContext(DbContextOptions<MeuDbContext> options) : base(options)
+        {
+            //algumas configs
+        }
+
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+        public DbSet<Fornecedor> Fornecedores { get; set; }
+    }
+}
