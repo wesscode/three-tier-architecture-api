007a97587b334cef796b077163f2bb3f6849ec67
Wesley Alves <wesley.info07@gmail.com>
2024-05-10 19:58:04 -0300

    Adicionando outros pacotes do EF e FK endereço.

--- src/ApiThreeTier.Business/Models/Endereco.cs
+++ src/ApiThreeTier.Business/Models/Endereco.cs
@@ -2,6 +2,7 @@
 {
     public class Endereco : Entity
     {
+        public Guid FornecedorId { get; set; }
         public string? Logradouro { get; set; }
         public string? Numero { get; set; }
         public string? Complemento { get; set; }
