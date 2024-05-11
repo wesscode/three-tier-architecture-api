243df934c51bcca721ce282c6608a2868bf08548
Wesley Alves <wesley.info07@gmail.com>
2024-05-10 23:21:52 -0300

    Limpando api

--- src/ApiThreeTier.API/Program.cs
+++ src/ApiThreeTier.API/Program.cs
@@ -3,10 +3,8 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -17,7 +15,6 @@ builder.Services.AddDbContext<MeuDbContext>(options =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
