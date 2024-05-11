ff18a2cc1a6118fa6c143521e621b134610fda5c
Wesley Alves <wesley.info07@gmail.com>
2024-05-10 23:02:28 -0300

    Implementando repositórios especializados.

---
+++ src/ApiThreeTier.Data/Repository/FornecedorRepository.cs
@@ -0,0 +1,39 @@
+﻿using ApiThreeTier.Business.Interfaces;
+using ApiThreeTier.Business.Models;
+using ApiThreeTier.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiThreeTier.Data.Repository
+{
+    public class FornecedorRepository : Repository<Fornecedor>, IFornecedorRepository
+    {
+        public FornecedorRepository(MeuDbContext context) : base(context) { }
+
+        public async Task<Fornecedor> ObterFornecedorEndereco(Guid id)
+        {
+            return await Db.Fornecedores.AsNoTracking()
+                .Include(c => c.Endereco)
+                .FirstOrDefaultAsync(c => c.Id == id);
+        }
+
+        public async Task<Fornecedor> ObterFornecedorProdutosEndereco(Guid id)
+        {
+          return await Db.Fornecedores.AsNoTracking()
+                .Include(f => f.Produtos)
+                .Include(e => e.Endereco)
+                .FirstOrDefaultAsync(c => c.Id == id);
+        }
+
+        public async Task<Endereco> ObterEnderecoPorFornecedor(Guid fornecedorId)
+        {
+            return await Db.Enderecos.AsNoTracking()
+                 .FirstOrDefaultAsync(f => f.FornecedorId == fornecedorId);
+        }
+
+        public async Task RemoverEnderecoFornecedor(Endereco endereco)
+        {
+             Db.Remove(endereco);
+            await SaveChanges();
+        }
+    }
+}
---
+++ src/ApiThreeTier.Data/Repository/ProdutoRepository.cs
@@ -0,0 +1,32 @@
+﻿using ApiThreeTier.Business.Interfaces;
+using ApiThreeTier.Business.Models;
+using ApiThreeTier.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiThreeTier.Data.Repository
+{
+    public class ProdutoRepository : Repository<Produto>, IProdutoRepository
+    {
+        public ProdutoRepository(MeuDbContext context) : base(context) { }
+
+        public async Task<Produto> ObterProdutoFornecedor(Guid id)
+        {
+            return await Db.Produtos.AsNoTracking()
+                .Include(x => x.Fornecedor)
+                .FirstOrDefaultAsync(p => p.Id == id);
+        }
+
+        public async Task<IEnumerable<Produto>> ObterProdutosFornecedor()
+        {
+            return await Db.Produtos.AsNoTracking()
+                .Include(x => x.Fornecedor)
+                .OrderBy(p => p.Nome)
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<Produto>> ObterProdutosPorFornecedor(Guid fornecedorId)
+        {
+            return await Buscar(p => p.FornecedorId == fornecedorId);
+        }
+    }
+}
